68267b2c89e91fae7d6de801ca908f770a682a99
Maciej Wieczorek <maciejmail.wieczorek@gmail.com>
2023-10-17 21:00:27 +0200

    init

---
+++ src/capturer.py
@@ -0,0 +1,35 @@
+import pyshark
+import time
+
+def capture_packets(interface, filter_expr, num_packets, timeout):
+    cap = pyshark.LiveCapture(interface=interface, display_filter=filter_expr, only_summaries=False)
+
+    sniff = cap.sniff_continuously(packet_count=num_packets)
+
+    next(sniff) # yield first packet before starting the timer
+    start_time = time.time()
+
+    for pkt in sniff:
+        if (time.time() - start_time) >= timeout:
+            break
+
+        try:
+            ip_src = pkt.ip.src
+            ip_dst = pkt.ip.dst
+            port_src = pkt[pkt.transport_layer].srcport
+            port_dst = pkt[pkt.transport_layer].dstport
+            packet_size = int(pkt.length)
+            timestamp = float(pkt.sniff_timestamp)
+            
+            # print(f"Timestamp: {timestamp}, Source: {ip_src}:{port_src}, Destination: {ip_dst}:{port_dst}, Packet Size: {packet_size}")
+            print(f"{timestamp}\t{ip_src}:{port_src}\t{ip_dst}:{port_dst}\t{packet_size}")
+        except AttributeError:
+            print("error")
+        
+if __name__ == "__main__":
+    interface = "Ethernet"
+    filter_expr = ""  # Wireshark filter expression (e.g., "tcp port 80")
+    num_packets = 1000  # Number of packets to capture
+    timeout = 5 # Timeout in seconds
+
+    capture_packets(interface, filter_expr, num_packets, timeout)
